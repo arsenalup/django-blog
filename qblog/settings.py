d681c110eb50e8e7ebded14f4b9c430199d4e327
chen keyan <676250063@qq.com>
2017-06-20 20:25:05 +0800

    www

--- qblog/settings.py
+++ qblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ahy=yj#2-hiroy8io*0*dy)p&5kedc8bq85pb&gcg#+#3r%ae='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
